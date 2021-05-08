8ccf665dc8908bab6fe9fc99003d09e9698be6d9
Swongliver <81005472+Swongliver@users.noreply.github.com>
2021-05-08 09:38:46 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+console.log("Hello, I am Scott");
+console.log("I don't know how to add or multiply in JavaScript")
+var b="15"
+
+var c="3"
+
+var a="The division of 15 by 3 is  "
+console.log(a);
+var divide=b / c;
+console.log(divide);
+
+var b="50"
+
+var c="2"
+
+var a="The multiplication of 50 and 2 is "
+console.log(a);
+var multiply1="50";
+var multiply2="2";
+var multiply3=multiply1 * multiply2;
+console.log(multiply3);
